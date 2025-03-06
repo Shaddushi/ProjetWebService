745720648e6e15f414d926d7c8c9189c232220e0
Shaddushi <94164129+Shaddushi@users.noreply.github.com>
2025-03-06 01:54:48 +0100

    Creation de dossier dans Entities specifique au différente partie du projet

--- BackEnd/Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
+++ BackEnd/Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bebdb10f09d88c85af06f89db482de59dddd2c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46c8517dad439ce5c38d335489be3bcd798f7e5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
